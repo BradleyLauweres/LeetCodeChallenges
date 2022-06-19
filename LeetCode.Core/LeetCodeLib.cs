a25509487c42c2ab41056aed262db8bfe65d6f84
Bradley Lauweres <Bradley.lauweres@student.howest.be>
2022-06-19 13:28:42 +0200

    Add RomanToInt Method

---
+++ LeetCode.Core/LeetCodeLib.cs
@@ -0,0 +1,42 @@
+﻿namespace LeetCodeName.Core
+{
+    public class LeetCodeLib
+    {
+        //Write a method that converts Roman input to a decimal number 
+
+        public int RomanToInt(string s)
+        {
+            var total = 0; 
+            var num = 0;
+
+            for(int i = s.Length -1; i >= 0; i--)
+            {
+                switch (s[i])
+                {
+                    case 'I': num = 1; break;
+                    case 'V': num = 5; break;
+                    case 'X': num = 10; break;
+                    case 'L': num = 50; break;
+                    case 'C': num = 100; break;
+                    case 'D': num = 500; break;
+                    case 'M': num = 1000; break;
+                }
+
+                if (4 * num < total) 
+                    total -= num;
+                else 
+                    total += num;
+            }
+
+            
+
+            return total;
+        }
+
+        public int IntToRoman(string s)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ LeetCode/program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCodeName.Core;
+
+class LeetCode
+{
+    static void Main(string[] args)
+    {
+        LeetCodeLib test = new LeetCodeLib();
+        Console.WriteLine(test.RomanToInt("III"));
+        Console.WriteLine(test.RomanToInt("LVIII"));
+        Console.WriteLine(test.RomanToInt("MCMXCIV"));
+    }
+}
